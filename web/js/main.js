59a271e00625d7ea91955eb667ce23db1c6ba444
j4mehta <jeet.mehta@kik.com>
2013-02-27 14:57:44 -0500

    Started categorizing, don't know how to load multiple feeds, tried modifying MyAPI.js but didn't work, need to refine how the shadow-work for the categories, code doesn't seem clean. Ask Justin/Jairaj about these 2 issues tmrw

--- web/js/main.js
+++ web/js/main.js
@@ -1,4 +1,4 @@
-App.populator('articleList', function (page) {
+App.populator('articleListnba', function (page) {
   MyAPI.getArticles(function (meta, articles) {
     logStuff(articles);
   });
@@ -57,6 +57,7 @@ App.populator('articleList', function (page) {
   var nhlBut = $(page).find('#nhl').clickable();
   var buttons = $(page).find('.category-button');
   var selectedButton = $(page).find('.selected-button');
+  var buttonClicked;
 
   buttons.on('click', function() {
     var clickedButton = $(page).find('#' + $(this).attr('id'));
@@ -66,6 +67,62 @@ App.populator('articleList', function (page) {
       clickedButton.addClass('selected-button');
       selectedButton = clickedButton;
       nbaBut.addClass('other-buttons');
+      console.log('articleList' + clickedButton.attr('id'));
+      App.load('articleList' + clickedButton.attr('id'), clickedButton);
+  });
+});
+
+App.populator('articleListnfl', function(page, clickedButton) {
+  MyAPI.getArticles(function (meta, articles) {
+    loadPage(articles);
+
+    function loadPage(data) {
+        console.log(data);
+        /*
+    data.forEach(function (item) {
+
+        var artTitle = item['title'];
+        var artSum = item['summary'];
+        var artDate = item['pubDate'];
+        var artLink = item['link'];
+        var imgLink = item['media:content']['@']['url'];
+
+        var section = $('<div />').addClass('app-section');
+        var description = $('<div />').addClass('description');
+        var title = $('<h4 />');
+        var summary = $('<div />').html(artSum);
+        var button = $('<div />').addClass('app-button myButtons');
+        var kikbutton = $('<div />').addClass('app-button myButtons');
+
+        $(page).find('.app-content').append(section);
+        section.append(description);
+        section.append(button);
+        section.append(kikbutton);
+        description.append(title);
+        description.append(summary);
+        
+        title.text(artTitle);
+        button.text('Read More');
+        kikbutton.text('Kik');
+
+        button.clickable().on('click', function() {
+          cards.browser.open(artLink);
+        });
+
+        kikbutton.clickable().on('click', function() {
+          var x = JSON.stringify(item);
+          console.log(x);
+          cards.kik.send({
+            title: artTitle,
+            text: 'Check out what I found!',
+            pic: imgLink,
+            big: false,
+            linkData: x
+          });
+        });
+    });
+*/
+  }
   });
 
 });
@@ -77,7 +134,7 @@ App.populator('articleView', function(page, linkData) {
   $(page).find('#backBut').text('Back');
 
   $(page).find('#backBut').clickable().on('click', function() {
-    App.load('articleList', 'slide-right');
+    App.load('articleListnba', 'slide-right');
   });
 
   $(page).find('#kikBut').clickable().on('click', function() {
@@ -108,6 +165,6 @@ if (cards.browser && cards.browser.linkData) {
   App.load('articleView', cards.browser.linkData);
 }
 else {
-  App.load('articleList');
+  App.load('articleListnba');
 }
 //Ideas: Setting max number of stories and dealing with old content messages, categorizing by sport
--- web/zerver/MyAPI.js
+++ web/zerver/MyAPI.js
@@ -1,5 +1,11 @@
-var feedparser = require('../../node_modules/feedparser');
+var feedparser1 = require('../../node_modules/feedparser');
+var feedparser2 = require('../../node_modules/feedparser');
 
 exports.getArticles = function (callback) {
-	feedparser.parseUrl('http://www.sportsnet.ca/basketball/index-pics-ontario.xml').on('complete', callback);
+	feedparser1.parseUrl('http://www.sportsnet.ca/basketball/index-pics-ontario.xml').on('complete', callback);
 };
+
+exports.getInfo = function (callback) {
+	feedparser2.parseUrl('http://www.sportsnet.ca/football/nfl/index-pics-ontario.xml').on('complete', callback);
+}
+
